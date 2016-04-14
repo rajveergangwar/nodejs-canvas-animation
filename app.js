10cd75cca9c609f88405a5aa82177887ba10d7e8
rajveer singh <rajveergangwar@2275692.no-reply.drupal.org>
2016-04-14 15:33:26 +0530

    simple multiplayer object display is complated

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var serverPlayerObj =   [];
-
-var player = function (width,height,startX,startY) {
-    this.width  = width;
-    this.height = height;
-    this.posX   = startX;
-    this.posY   = startY;
-    this.id     =   '';
-}
-
+var SOCKET_LIST =   [];
 app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
 io.on('connection', function (socket) {
-    var p1  =   new player();
-    p1.width= 30;
-    p1.height=30;
-    p1.posX=rand(0,1);
-    p1.posY=rand(100,400);
-    p1.id=socket.id;
-    serverPlayerObj.push(p1);
-    socket.emit('new_player', serverPlayerObj);
-    console.log('a user connected');
+    socket.id   =   Math.random();
+    socket.x    =   0;
+    socket.y    =   0;
+    SOCKET_LIST[socket.id]  =   socket;
 });
+
+setInterval(function(){
+    var holder  =   [];
+    for(var i in SOCKET_LIST) {
+        var socket  =   SOCKET_LIST[i];
+        socket.x++;
+        if(socket.y!=480)
+            socket.y++;
+        else
+            socket.y    = 0;
+        
+        holder.push({
+            x:socket.x,
+            y:socket.y,
+        });
+    }
+    
+    for(var i in SOCKET_LIST){
+        var socket  =   SOCKET_LIST[i];
+        socket.emit('new_position',holder);
+    }
+    
+},1000/25);
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
-
-
-function rand(min,max,interval)
-{
-    if (typeof(interval)==='undefined') interval = 40;
-    var r = Math.floor(Math.random()*(max-min+interval)/interval);
-    return r*interval+min;
-}
