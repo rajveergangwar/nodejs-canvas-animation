fef49e2768b64b2c68028640c9c02ab530ff6514
rajveer singh <rajveergangwar@2275692.no-reply.drupal.org>
2016-04-19 15:59:28 +0530

    collision detection is done successfully

--- app.js
+++ app.js
@@ -1,53 +1,72 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var canvas_data =   {cWidth:640,cHeight:480};
-var SOCKET_LIST =   [];
+var canvas_data = {cWidth: 640, cHeight: 480};
+var SOCKET_LIST = [];
 app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
 io.on('connection', function (socket) {
-    socket.on('canvas_data',function(data){
-        canvas_data.cWidth      =   data.width;
-        canvas_data.cHeight     =   data.height;
+    socket.id = Math.random();
+    socket.x = getRandomArbitrary(0, 640);
+    socket.y = 0;
+    socket.Iscolled = false;
+    socket.num = getRandomColor();
+    socket.on('canvas_data', function (data) {
+        canvas_data.cWidth = data.width;
+        canvas_data.cHeight = data.height;
     });
-    socket.id   =   Math.random();
-    socket.x    =  getRandomArbitrary(0,640);
-    socket.y    =   0;
-    socket.num  =   Math.floor((Math.random()*9) + 1);
-    SOCKET_LIST[socket.id]  =   socket;
-    
-    
-    socket.on('disconnect',function(){
-       delete SOCKET_LIST[socket.id]; 
+
+    socket.on("collision_detacted", function (collied_data) {
+        socket.Iscolled = true;
+    });
+    SOCKET_LIST[socket.id] = socket;
+
+
+    socket.on('disconnect', function () {
+        delete SOCKET_LIST[socket.id];
     });
 });
 
-setInterval(function(){
-    var holder  =   [];
-    for(var i in SOCKET_LIST) {
-        var socket  =   SOCKET_LIST[i];
-        if(socket.y!=canvas_data.cHeight) {
-            socket.y    +=5;
-        }else {
-            socket.y        =   0;
-            socket.x    =  getRandomArbitrary(0,640);
-            socket.num      =   Math.floor((Math.random()*9) + 1);
+setInterval(function () {
+    var holder = [];
+    for (var i in SOCKET_LIST) {
+
+
+        var socket = SOCKET_LIST[i];
+        if (socket.y != canvas_data.cHeight) {
+            if (socket.Iscolled) {
+                socket.y += 10;
+                if (socket.y >= 480) {
+                    socket.y = 0;
+                    socket.x = getRandomArbitrary(0, 640);
+                    socket.num = getRandomColor();
+                    socket.Iscolled = false;
+                }
+                console.log(socket.y);
+            }
+            else {
+                socket.y += 1;
+            }
+        } else {
+            socket.y = 0;
+            socket.x = getRandomArbitrary(0, 640);
+            socket.Iscolled = false;
+            socket.num = getRandomColor();
         }
         holder.push({
-            x:socket.x,
-            y:socket.y,
-            num:socket.num,
+            x: socket.x,
+            y: socket.y,
+            num: socket.num,
         });
     }
-    
-    for(var i in SOCKET_LIST){
-        var socket  =   SOCKET_LIST[i];
-        socket.emit('new_position',holder);
+    for (var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i];
+        socket.emit('new_position', holder);
     }
-    
-},1000/25);
+
+}, 1000 / 25);
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
@@ -57,5 +76,20 @@ http.listen(3000, function () {
  * Generating the ramdom number beween the range
  */
 function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
+}
+
+/*
+ * ge random color
+ */
+
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
