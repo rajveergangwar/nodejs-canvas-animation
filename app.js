b5477ff8b0a94b2d08054deeb40cc32d11548101
rajveer singh <rajveergangwar@2275692.no-reply.drupal.org>
2016-04-21 19:07:17 +0530

    working on fixing intersection of click and ball

--- app.js
+++ app.js
@@ -6,67 +6,72 @@ var SOCKET_LIST = [];
 app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
-
+var test    =   [];
 io.on('connection', function (socket) {
-    socket.id = Math.random();
-    socket.x = getRandomArbitrary(0, 640);
-    socket.y = 0;
+    var k           =   [];
+    socket.objId    = socket.id;
+    socket.x        = getRandomArbitrary(0, 640);
+    socket.y        = 0;
     socket.Iscolled = false;
-    socket.num = getRandomColor();
+    socket.num      = getRandomColor();
     socket.on('canvas_data', function (data) {
         canvas_data.cWidth = data.width;
         canvas_data.cHeight = data.height;
     });
-
+    
+    socket.emit("info",socket.objId);
+    
     socket.on("collision_detacted", function (collied_data) {
-        socket.Iscolled = true;
+        socket.Iscolled     =   true;
+        socket.objId        =   collied_data.socketId;
     });
-    SOCKET_LIST[socket.id] = socket;
+    SOCKET_LIST[socket.objId] = socket;
 
-
+    //console.log(SOCKET_LIST);
     socket.on('disconnect', function () {
-        delete SOCKET_LIST[socket.id];
+        delete SOCKET_LIST[socket.objId];
     });
 });
 
 setInterval(function () {
     var holder = [];
     for (var i in SOCKET_LIST) {
-
-
         var socket = SOCKET_LIST[i];
-        if (socket.y != canvas_data.cHeight) {
-            if (socket.Iscolled) {
-                socket.y += 10;
-                if (socket.y >= 480) {
-                    socket.y = 0;
-                    socket.x = getRandomArbitrary(0, 640);
-                    socket.num = getRandomColor();
-                    socket.Iscolled = false;
-                }
-                console.log(socket.y);
+            if (socket.y != canvas_data.cHeight) {
+                if (socket.Iscolled) {
+                    socket.y += 10;
+                    if (socket.y >= 480) {
+                        socket.y = 0;
+                        socket.objId    =   i;
+                        socket.x = getRandomArbitrary(0, 640);
+                        socket.num = getRandomColor();
+                        socket.Iscolled = false;
+                    }
             }
             else {
                 socket.y += 1;
             }
         } else {
-            socket.y = 0;
-            socket.x = getRandomArbitrary(0, 640);
-            socket.Iscolled = false;
-            socket.num = getRandomColor();
+            socket.y        =   0;
+            socket.x        =   getRandomArbitrary(0, 640);
+            socket.Iscolled =   false;
+            socket.num      =   getRandomColor();
+            socket.objId    =   i;
         }
         holder.push({
             x: socket.x,
             y: socket.y,
             num: socket.num,
+            id:i,
         });
     }
     for (var i in SOCKET_LIST) {
         var socket = SOCKET_LIST[i];
+        console.log(holder);
         socket.emit('new_position', holder);
     }
 
-}, 1000 / 25);
+}, 1000/40);
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
